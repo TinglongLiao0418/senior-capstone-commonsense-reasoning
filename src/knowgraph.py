e32b33a31a6614f2ffaeefd6f1624ec2d995d882
Yuqin Bai <yb1033@nyu.edu>
2022-03-24 21:48:17 -0400

    Add Knowledge Graph

---
+++ src/knowgraph.py
@@ -0,0 +1,146 @@
+# coding: utf-8
+"""
+Based on K-BERT KnowledgeGraph
+by Weijie Liu, Peng Zhou, Zhe Zhao, Zhiruo Wang, Qi Ju, Haotang Deng, Ping Wang
+"""
+import os
+import src.config as config
+import numpy as np
+import random
+
+
+class KnowledgeGraph(object):
+
+    def __init__(self, knowledge_path, predicate=False):
+        self.predicate = predicate
+        self.knowledge_path = knowledge_path
+        self.lookup_table = self._create_lookup_table()
+        self.segment_vocab = list(self.lookup_table.keys()) + config.NEVER_SPLIT_TAG
+        self.special_tags = set(config.NEVER_SPLIT_TAG)
+
+    def _create_lookup_table(self):
+        """
+        Use retrieved ConceptNet as KG
+        """
+        lookup_table = {}
+        with open(self.knowledge_path, 'r', encoding='utf-8') as f:
+            for line in f:
+                try:
+                    subj, pred, obje = line.strip().split("\t")    
+                except:
+                    print("[KnowledgeGraph] Bad json:", line)
+                if self.predicate:
+                    value = "{} {}".format(pred, obje)
+                else:
+                    value = obje
+                if subj in lookup_table.keys():
+                    lookup_table[subj].add(value)
+                else:
+                    lookup_table[subj] = set([value])
+        return lookup_table
+
+    def add_knowledge_with_vm(self, data_batch, max_entities=config.MAX_ENTITIES, add_pad=True, max_length=128):
+        """
+        input: data_batch
+        return: know_sent_batch - list of sentences with entites embedding
+                position_batch - list of position index of each character.
+                visible_matrix_batch - list of visible matrixs
+                seg_batch - list of segment tags
+        """
+        know_sent_batch = []
+        position_batch = []
+        visible_matrix_batch = []
+        seg_batch = []
+
+        for data_sample in data_batch:
+
+            # create tree
+            sent_tree = []
+            pos_idx_tree = []
+            abs_idx_tree = []
+            pos_idx = -1
+            abs_idx = -1
+            abs_idx_src = []
+
+            token = data_sample["topic_prompt"]
+
+            entities = list(self.lookup_table.get(token, []))
+            random.shuffle(entities)
+            sent_tree.append((token, entities[:max_entities]))
+
+            if token in self.special_tags:
+                token_pos_idx = [pos_idx+1]
+                token_abs_idx = [abs_idx+1]
+            else:
+                token_pos_idx = [pos_idx+i for i in range(1, len(token)+1)]
+                token_abs_idx = [abs_idx+i for i in range(1, len(token)+1)]
+            abs_idx = token_abs_idx[-1]
+
+            entities_pos_idx = []
+            entities_abs_idx = []
+            for ent in entities[:max_entities]:
+                ent_pos_idx = [token_pos_idx[-1] + i for i in range(1, len(ent)+1)]
+                entities_pos_idx.append(ent_pos_idx)
+                ent_abs_idx = [abs_idx + i for i in range(1, len(ent)+1)]
+                abs_idx = ent_abs_idx[-1]
+                entities_abs_idx.append(ent_abs_idx)
+
+            pos_idx_tree.append((token_pos_idx, entities_pos_idx))
+            pos_idx = token_pos_idx[-1]
+            abs_idx_tree.append((token_abs_idx, entities_abs_idx))
+            abs_idx_src += token_abs_idx
+
+            # Get know_sent and pos
+            know_sent = []
+            pos = []
+            seg = []
+            for i in range(len(sent_tree)):
+                word = sent_tree[i][0]
+                if word in self.special_tags:
+                    know_sent += [word]
+                    seg += [0]
+                else:
+                    add_word = list(word)
+                    know_sent += add_word 
+                    seg += [0] * len(add_word)
+                pos += pos_idx_tree[i][0]
+                for j in range(len(sent_tree[i][1])):
+                    add_word = list(sent_tree[i][1][j])
+                    know_sent += add_word
+                    seg += [1] * len(add_word)
+                    pos += list(pos_idx_tree[i][1][j])
+
+            token_num = len(know_sent)
+
+            # Calculate visible matrix
+            visible_matrix = np.zeros((token_num, token_num))
+            for item in abs_idx_tree:
+                src_ids = item[0]
+                for id in src_ids:
+                    visible_abs_idx = abs_idx_src + [idx for ent in item[1] for idx in ent]
+                    visible_matrix[id, visible_abs_idx] = 1
+                for ent in item[1]:
+                    for id in ent:
+                        visible_abs_idx = ent + src_ids
+                        visible_matrix[id, visible_abs_idx] = 1
+
+            src_length = len(know_sent)
+            if len(know_sent) < max_length:
+                pad_num = max_length - src_length
+                know_sent += [config.PAD_TOKEN] * pad_num
+                seg += [0] * pad_num
+                pos += [max_length - 1] * pad_num
+                visible_matrix = np.pad(visible_matrix, ((0, pad_num), (0, pad_num)), 'constant')  # pad 0
+            else:
+                know_sent = know_sent[:max_length]
+                seg = seg[:max_length]
+                pos = pos[:max_length]
+                visible_matrix = visible_matrix[:max_length, :max_length]
+            
+            know_sent_batch.append(know_sent)
+            position_batch.append(pos)
+            visible_matrix_batch.append(visible_matrix)
+            seg_batch.append(seg)
+        
+        return know_sent_batch, position_batch, visible_matrix_batch, seg_batch
+

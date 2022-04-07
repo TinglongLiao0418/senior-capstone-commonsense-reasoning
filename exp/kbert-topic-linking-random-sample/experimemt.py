20bd543f3b484808ccf24ab6c458c955d80cd173
TinglongLiao0418 <tinglongliao@nyu.edu>
2022-04-06 23:39:29 -0400

    Delete KBert customized model
    HuggingFace already provides interface to pass in 3-D attention mask, no need to create own

---
+++ exp/kbert-topic-linking-random-sample/experimemt.py
@@ -0,0 +1,26 @@
+from transformers import AutoTokenizer, BertConfig, BertForSequenceClassification
+
+from src.dataset import CSQA2DatasetWithVisibleMatrix
+from src.trainer import run_experiment
+# from src.model import KBertForSequenceClassification
+
+if __name__ == '__main__':
+    config = {'max_seq_length': 512, 'max_entities': 10}
+    model_config = BertConfig(num_labels=2)
+    tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
+    train_dataset = CSQA2DatasetWithVisibleMatrix(config=config,
+                                                  data_path="../../data/csqa2/train.json",
+                                                  tokenizer=tokenizer,
+                                                  knowledge_path="../../data/knowledge/conceptnet.csv")
+    eval_dataset = CSQA2DatasetWithVisibleMatrix(config=config,
+                                                  data_path="../../data/csqa2/dev.json",
+                                                  tokenizer=tokenizer,
+                                                  knowledge_path="../../data/knowledge/conceptnet.csv")
+    model = BertForSequenceClassification.from_pretrained('bert-base-uncased')
+    run_experiment(
+        model=model,
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
+        data_collator=train_dataset.collate_fn,
+        output_dir=".",
+    )

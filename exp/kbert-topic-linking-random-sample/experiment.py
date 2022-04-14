266f2083acdc60c837329c02db00bbf299494b84
TinglongLiao0418 <tinglongliao@nyu.edu>
2022-04-13 23:33:21 -0400

    Update experiment.py

--- exp/kbert-topic-linking-random-sample/experiment.py
+++ exp/kbert-topic-linking-random-sample/experiment.py
@@ -8,7 +8,8 @@ from src.trainer import run_experiment
 
 if __name__ == '__main__':
 
-    config = {'max_seq_length': 512, 'max_entities': 10}
+    config = {'max_seq_length': 512, 'max_entities': 80, entity_sample='weighted'}
+    print(config)
     model_config = BertConfig(num_labels=2)
     tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
     train_dataset = CSQA2DatasetWithVisibleMatrix(config=config,

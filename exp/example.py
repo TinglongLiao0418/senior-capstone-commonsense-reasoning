c5c328ba7b34d36d8cfa03b63ae1fd9ba71efb92
TinglongLiao0418 <tinglongliao@nyu.edu>
2022-04-13 23:52:43 -0400

    Bug fix

--- exp/example.py
+++ exp/example.py
@@ -1,16 +1,25 @@
-from transformers import AutoTokenizer
-from transformers import BertForSequenceClassification
+import sys
 
-from src.dataset import CSQA2DatasetBase
+project_path = '/'.join(sys.path[0].split('/')[:sys.path[0].split('/').index('senior-capstone-commonsense-reasoning') + 1])
+sys.path.insert(1, project_path)
+from transformers import AutoTokenizer, BertConfig, BertForSequenceClassification
+
+from src.dataset import CSQA2DatasetWithVisibleMatrix
 from src.trainer import run_experiment
 
-# from src.model import KBERT
-
 if __name__ == '__main__':
-    config = {'max_seq_length': 512}
+    config = {'max_seq_length': 512, 'max_entities': 20, 'entity_sample': 'weighted'}
+    print(config)
+    model_config = BertConfig(num_labels=2)
     tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
-    train_dataset = CSQA2DatasetBase(config, "../data/csqa2/train.json", tokenizer)
-    eval_dataset = CSQA2DatasetBase(config, "../data/csqa2/dev.json", tokenizer)
+    train_dataset = CSQA2DatasetWithVisibleMatrix(config=config,
+                                                  data_path="../../data/csqa2/train.json",
+                                                  tokenizer=tokenizer,
+                                                  knowledge_path="../../data/knowledge/conceptnet.csv")
+    eval_dataset = CSQA2DatasetWithVisibleMatrix(config=config,
+                                                 data_path="../../data/csqa2/dev.json",
+                                                 tokenizer=tokenizer,
+                                                 knowledge_path="../../data/knowledge/conceptnet.csv")
     model = BertForSequenceClassification.from_pretrained('bert-base-uncased')
     run_experiment(
         model=model,
@@ -19,3 +28,4 @@ if __name__ == '__main__':
         data_collator=train_dataset.collate_fn,
         output_dir=".",
     )
+

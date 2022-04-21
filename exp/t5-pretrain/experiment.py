4659b9b6e0d50efe4f67d9f13e32beaff26afc36
TinglongLiao0418 <tinglongliao@nyu.edu>
2022-04-21 17:31:18 -0400

    Change pretrain arg

--- exp/t5-pretrain/experiment.py
+++ exp/t5-pretrain/experiment.py
@@ -8,7 +8,7 @@ from src.trainer import run_experiment
 
 if __name__ == '__main__':
 
-    config = {'model': 't5-large', 'max_seq_length': 512, 'max_entities': 40}
+    config = {'model': 't5-large'}
     tokenizer = T5Tokenizer.from_pretrained(config['model'])
     train_dataset = CorruptedConceptNet(path="../../data/knowledge/conceptnet.csv", tokenizer=tokenizer)
     eval_dataset = CorruptedConceptNet(path="../../data/knowledge/conceptnet.csv", tokenizer=tokenizer)
@@ -17,9 +17,10 @@ if __name__ == '__main__':
         model=model,
         train_dataset=train_dataset,
         eval_dataset=eval_dataset,
-        per_device_train_batch_size=4,
-        per_device_eval_batch_size=4,
-        gradient_accumulation_steps=2,
+        epoch=1,
+        per_device_train_batch_size=8,
+        per_device_eval_batch_size=8,
+        gradient_accumulation_steps=1,
         data_collator=train_dataset.collate_fn,
         output_dir="log",
         learning_rate=1e-4
